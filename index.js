a0d8b5e517bf6fe991039bd6f6bf525681b5bfa4
TaiwoDaniel <Taiwodaniel3116@mail.com>
2025-02-10 06:45:11 -0800

    add square content functionality

--- index.js
+++ index.js
@@ -9,14 +9,21 @@ hamburgerMenu.addEventListener('click', () => {
 //
 
 
-  const designBtn = document.querySelector('.design-btn');
+ const designBtn = document.querySelector('.design-btn');
 const codeBtn = document.querySelector('.code-btn');
 const textBtn = document.querySelector('.text-btn');
+const squareContent1 = document.querySelector('.square-content1');
+const squareContent2 = document.querySelector('.square-content2');
+const squareContent3 = document.querySelector('.square-content3');
 
 function designSwitch() {
    if (designBtn.classList.contains('input-btns')) {
      designBtn.classList.remove('input-btns');
      designBtn.classList.add('design-btn');
+
+     squareContent1.style.display = "block";
+     squareContent2.style.display = "none";
+     squareContent3.style.display = "none";
 
      codeBtn.classList.add('input-btns');
      codeBtn.classList.remove('code-btn');
@@ -31,9 +38,13 @@ function designSwitch() {
 }
 
 function codeSwtich() {
-  if (codeBtn.classList.contains('input-btns') ) {
+  if (codeBtn.classList.contains('input-btns')) {
     codeBtn.classList.remove('input-btns');
     codeBtn.classList.add('code-btn');
+
+    squareContent1.style.display = "none";
+    squareContent2.style.display = "block";
+    squareContent3.style.display = "none";
 
     designBtn.classList.remove('design-btn');
     designBtn.classList.add('input-btns');
@@ -48,9 +59,13 @@ function codeSwtich() {
 }
 
 function textSwtich() {
-  if (textBtn.classList.contains('input-btns') ) {
+  if (textBtn.classList.contains('input-btns')) {
      textBtn.classList.remove('input-btns');
      textBtn.classList.add('text-btn');
+
+     squareContent1.style.display = "none";
+     squareContent2.style.display = "none";
+     squareContent3.style.display = "block";
 
      codeBtn.classList.add('input-btns');
      codeBtn.classList.remove('code-btn');
